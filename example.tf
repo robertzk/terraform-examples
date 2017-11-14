ff6c190eb3c71cfa5d4612bc5541df31e2baaf22
robertzk <technoguyrob@gmail.com>
2017-11-13 21:49:59 -0600

    Some elastic IPS

--- example.tf
+++ example.tf
@@ -1,11 +1,25 @@
 provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
   region     = "us-east-1"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-2757f631"
+  ami           = "ami-b374d5a5"
   instance_type = "t2.micro"
+  key_name      = "terraform-examples"
 }
+
+resource "aws_instance" "another" {
+  ami           = "ami-b374d5a5"
+  instance_type = "t2.micro"
+  key_name      = "terraform-examples"
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  }
+}
+
+resource "aws_eip" "example-ip" {
+  instance = "${aws_instance.example.id}"
+}
+
 
